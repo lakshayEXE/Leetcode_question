714710a770edecabf9714e236b148f3bcbc894e2
Lakshay Bhatia <97439907+lakshayEXE@users.noreply.github.com>
2025-10-27 23:36:30 +0530

    Time: 3 ms (27.80%) | Memory: 97.3 MB (86.79%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -1,49 +1,14 @@
 class Solution {
 public:
-    int maxProfit(vector<int>& nums) {
-
-        // int mini =nums[0];
-        // int profit = 0;
-
-        // for(int i = 1 ; i< nums.size();i++){
-        //     mini =min(mini,nums[i]);
-        //     profit = max(nums[i] - mini,profit);
-        // }
-        // return profit;
-
-
-        // int profit = 0 ;
-        // for(int i = 0; i < nums.size() ; i++){
-        //     int buyprice = nums[i];
-
-        //     for(int j = i+1 ;j<nums.size() ;j++){
-        //         profit = max(profit , nums[j] - buyprice);
-        //     }
-        // }
-
-        // return profit;
-
-        // int profit = 0;
-        // int cp = INT_MAX;
-
-        // for(int i = 0 ; i < nums.size() ; i++){
-        //     cp = min(nums[i] ,cp );
-        //     profit = max(profit , nums[i] - cp );
-        // }
-        // return profit;
-
-
-
-        int buyPrice = INT_MAX;
-        int profit = INT_MIN;
-        for(int i =0; i<nums.size() ;i++){
-            if(buyPrice > nums[i]){
-                buyPrice = nums[i];
-            }
-            int currProfit = nums[i]-buyPrice;
-            profit = max(profit , currProfit);
+    int maxProfit(vector<int>& prices) {
+        int mini = INT_MAX;
+        int profit  =0 ;
+        int curr_profit = 0;
+        for(int i= 0; i<prices.size() ;i++){
+            mini = min(mini,prices[i]);
+            curr_profit = prices[i]-mini;
+            profit = max(curr_profit , profit);
         }
         return profit;
-
     }
 };
